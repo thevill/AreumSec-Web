ca984a3ea316f1c8c91ba21c9dfb0d6fcd1190ec
Th3vi11 <44583332+thevill@users.noreply.github.com>
2025-05-04 14:29:32 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-class Config:
-    VIRUSTOTAL_API_KEY = 'add_your_key_here'
-    GOOGLE_SAFE_BROWSING_API_KEYS = ['add_your_key_here'
-    ]
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
